7f622bdc8cb873f11b38f9e66e199275eb637198
Blackinfinityro <megalucario97r@gmail.com>
2025-04-27 00:17:02 +0200

    aggiunta file al commit

---
+++ MiTtify/Migrations/MiTtify/20250417214444_AggiuntaFilePathCanzone.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiTtify.Migrations.MiTtify
+{
+    /// <inheritdoc />
+    public partial class AggiuntaFilePathCanzone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "Canzone",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "Canzone");
+        }
+    }
+}
---
+++ MiTtify/Migrations/MiTtify/20250422081800_riaggiustamento.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiTtify.Migrations.MiTtify
+{
+    /// <inheritdoc />
+    public partial class riaggiustamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
