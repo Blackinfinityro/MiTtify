2d262ab46560f649b5bc540ddd791b9e52522c08
Blackinfinityro <megalucario97r@gmail.com>
2025-05-22 17:41:11 +0200

    aggiunta file e directory?

---
+++ MiTtify/Migrations/MiTtify/20250507214524_immagineCanzone.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiTtify.Migrations.MiTtify
+{
+    /// <inheritdoc />
+    public partial class immagineCanzone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Canzone",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Canzone");
+        }
+    }
+}
---
+++ MiTtify/Migrations/MiTtify/20250514205354_implemantationGenreMusic.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiTtify.Migrations.MiTtify
+{
+    /// <inheritdoc />
+    public partial class implemantationGenreMusic : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstGenre",
+                table: "Canzone",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecondGenre",
+                table: "Canzone",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstGenre",
+                table: "Canzone");
+
+            migrationBuilder.DropColumn(
+                name: "SecondGenre",
+                table: "Canzone");
+        }
+    }
+}
